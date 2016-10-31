f832d0fec257f88108ef7b914d651ad71fcc4533
Dario Arias <h.dario95@gmail.com>
2016-10-31 10:52:34 -0500

    Hola mundo

--- ejemploSumaResta.java
+++ ejemploSumaResta.java
@@ -5,15 +5,16 @@ public class ejemploSumaResta {
 	public static void main(String[] args) {
 		Scanner scnIngreso = new Scanner(System.in);
 		int a=0, b=0, r=0;
-		
-		
+
+
 		System.out.println("Ingrese A: ");
 		a=scnIngreso.nextInt();
 		System.out.println("Ingrese B: ");
 		b=scnIngreso.nextInt();
-		
+
 		if (a>=b){
 			r=a-b;
+			System.out.println("Hola mundo");
 			System.out.println(" R= "+a+" - "+b+" = "+r);
 		}
 		else
@@ -23,7 +24,7 @@ public class ejemploSumaResta {
 			System.out.println(" R= "+a+" + "+b+" = "+r);
 			System.out.println(" R= "+a+" + "+b+" = "+r);
 		}
-		
-			
+
+
 	}
 }
