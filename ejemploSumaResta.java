ed4fd9ebd216f5626cbc266f8ac7c1baeaab4493
AndresDLTZ <delatorre.an@gmail.com>
2016-10-31 09:45:54 -0500

    Add files via upload

---
+++ ejemploSumaResta.java
@@ -0,0 +1,27 @@
+package ejemploSumaResta;
+
+import java.util.Scanner;
+public class ejemploSumaResta {
+	public static void main(String[] args) {
+		Scanner scnIngreso = new Scanner(System.in);
+		int a=0, b=0, r=0;
+		
+		
+		System.out.println("Ingrese A: ");
+		a=scnIngreso.nextInt();
+		System.out.println("Ingrese B: ");
+		b=scnIngreso.nextInt();
+		
+		if (a>=b){
+			r=a-b;
+			System.out.println(" R= "+a+" - "+b+" = "+r);
+		}
+		else
+		{
+			r=a+b;
+			System.out.println(" R= "+a+" + "+b+" = "+r);
+		}
+		
+			
+	}
+}
